144122d3b6ab584e249f97ce37023a53870357ee
shrooot <yashrajsingh977@gmail.com>
2024-01-10 12:09:09 +0530

    Add csv conversion

--- scraper.py
+++ scraper.py
@@ -5,8 +5,14 @@ import numpy as np
 from google_play_scraper import Sort, reviews_all, reviews
 
 def get_all_reviews(lang, country, sort):
-   return reviews("com.branch_international.branch.branch_demo_android", lang=lang, country=country, sort=sort, count = 10000)
+   all_reviews, _continuation_token = reviews("com.branch_international.branch.branch_demo_android", lang=lang, country=country, sort=sort, count = 15000)
+   print(len(all_reviews))
 
-reviews, _continuation_token = get_all_reviews('en', 'in', Sort.NEWEST)
+   df_reviews = pd.DataFrame(np.array(all_reviews), columns=["review"])
+   df_reviews = df_reviews.join(pd.DataFrame(df_reviews.pop('review').tolist()))
+   df_reviews = df_reviews[['content']]
 
-print(len(reviews))
+   df_reviews.to_csv("in-playstore-reviews.csv")
+
+if __name__ == "__main__":
+   get_all_reviews('en', 'in', Sort.NEWEST)
